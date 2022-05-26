c3cd2e53eefd04c44c9014c98474d000ab302b6b
skugerne <31828871+skugerne@users.noreply.github.com>
2022-05-26 19:09:46 +0200

    Some files.

---
+++ skuggMod.sdd/modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'The Skugg Zero-K Mod',
+	description = 'A mod to explore ideas and screw around',
+	shortname = 'skuggmod',
+	version = 'v1',
+	mutator = '1',
+	game = '',
+	shortGame = '',
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
